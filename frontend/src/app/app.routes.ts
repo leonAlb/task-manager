6158d2e0fe4615d2180f7043b769b6d16b06d877
Albert, Leon <leon.albert@study.thws.de>
2026-04-19 19:09:16 +0200

    Frontend setup of routes pages and services

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    title: 'Login Page',
+    loadComponent: () => import('./pages/login/login').then(m => m.Login),
+  },
+  {
+    path: 'register',
+    title: 'Register Page',
+    loadComponent: () => import('./pages/register/register').then(m => m.Register),
+  },
+  {
+    path: 'tasks',
+    title: 'Tasks Page',
+    loadComponent: () => import('./pages/tasks/tasks').then(m => m.Tasks),
+  },
+];
