1d419db390a56ddb36b8cda26252a101ca7f1735
Albert, Leon <leon.albert@study.thws.de>
2026-04-23 16:59:51 +0200

    Added styling for login and register pages

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth';
+import { ThemeService } from './services/theme';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,7 @@ import { AuthService } from './services/auth';
 })
 export class App implements OnInit {
   private authService = inject(AuthService);
+  protected themeService = inject(ThemeService);
 
   ngOnInit() {
     if (this.authService.isLoggedIn()) {
